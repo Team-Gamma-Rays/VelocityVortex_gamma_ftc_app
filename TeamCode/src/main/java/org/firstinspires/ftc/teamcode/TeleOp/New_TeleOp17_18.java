cd7f74172b244102c6f3325ed2fa7b0c94d7f8c9
ACHS-Gamma-Rays <ftcgammarays@gmail.com>
2017-12-07 15:53:00 -0500

    Redesign of TeleOp
    The new version of TeleOp uses drive control methods from the Robot_HoloDrive class based on Gears Inc's FTCVuforiaDemo.
    D-pad control is added.
    Needs to be tested as of 12/7/2017
    
    -Richie

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/New_TeleOp17_18.java
@@ -0,0 +1,105 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Auto.Robot_HoloDrive;
+import org.firstinspires.ftc.teamcode.Hardware.Hardware17_18;
+
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Dr. Jones TeleOp 2.0", group="TeleOp")
+//@Disabled
+public class New_TeleOp17_18 extends LinearOpMode
+{
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    Robot_HoloDrive drive = new Robot_HoloDrive();
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void runOpMode() {
+        // Initialize the robot and navigation
+        drive.initDrive(this);
+
+        // Wait for the game to start (driver presses PLAY)
+        while (!isStarted()) {
+            // Prompt User
+            telemetry.addData(">", "Press start");
+        }
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            telemetry.addData(">", "Press Left Bumper to track target");
+
+            // In manual drive the robot responds to the Joystick.
+
+            // Drive the robot using the joysticks
+            drive.manualDrive();
+
+            // D - pad Control
+            if (gamepad1.dpad_up) {
+                drive.moveRobot(0.5, 0, 0);
+            } else if (gamepad1.dpad_down) {
+                drive.moveRobot(-0.5, 0, 0);
+            } else if (gamepad1.dpad_left) {
+                drive.moveRobot(0, -0.5, 0);
+            } else if (gamepad1.dpad_right) {
+                drive.moveRobot(0, 0.5, 0);
+            }
+
+            //  Move the robot according to the pre-determined axis motions
+            drive.moveRobot();
+            telemetry.update();
+        }
+
+        telemetry.addData(">", "Shutting Down. Bye!");
+        telemetry.update();
+    }
+
+}
