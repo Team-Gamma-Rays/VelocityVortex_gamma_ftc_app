c585fc90f9a22fb09f9dc1fa38b51f764d044bed
ACHS-Gamma-Rays <ftcgammarays@gmail.com>
2017-12-22 21:52:22 -0500

    Added OpMode to test arm lifting motor that uses an encoder to reset position
    - Richie

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/OneMotorArmLiftTest_Encoder.java
@@ -0,0 +1,112 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * ADDED BY GAMMA RAYS:
+ * This OpMode mode is based upon the PushbotTeleopPOV_Linear class.
+ * It uses the common HardwareOurRobot hardware class to define the devices on the robot.
+ *
+ * This OpMode is designed to test the servos in Sprocket's(our team's robot) original arm configuration.
+ * It can be used to determine the optimal servo position values for initial positions
+ * and preset servo movements in both Teleop and Autonomous OpModes.
+ * In this mode:
+ * The X and Y axes of the Left stick move the Horizontal and Vertical arm servos on the Port side of the robot respectively.
+ * The Right stick performs the same function, but for arm servos on the Starboard side.
+ *
+ * All servo positions are sent to the telemetry display on the Driver Station phone.
+ */
+
+/**
+ * ORIGINAL COMMENT:
+ * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
+ * All device access is managed through the HardwarePushbot class.
+ * The code is structured as a LinearOpMode
+ *
+ * This particular OpMode executes a POV Game style Teleop for a PushBot
+ * In this mode the left stick moves the robot FWD and back, the Right stick turns left and right.
+ * It raises and lowers the claw using the Gampad Y and A buttons respectively.
+ * It also opens and closes the claws slowly using the left and right Bumper buttons.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Claw Servo Test", group="Testing")
+//@Disabled
+public class OneMotorArmLiftTest_Encoder extends LinearOpMode {
+
+    DcMotor armLift  = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        armLift = hardwareMap.get(DcMotor.class, "arm_lift_motor");
+        final double armLiftPower    = 0.2; //Set power to move motor
+        final int    armLiftHome     = 0; //Encoder value used to move arm to a default init position
+
+        //Init for arm lift motor
+        armLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armLift.setTargetPosition(armLiftHome);
+        armLift.setPower(armLiftPower);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            armLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            int position = armLift.getCurrentPosition();
+
+            if (gamepad1.left_bumper)
+                armLift.setPower(-armLiftPower);
+            else if (gamepad1.right_bumper)
+                armLift.setPower(armLiftPower);
+
+            //Send telemetry messages corresponding to servo positions.
+            telemetry.addData("armLiftPower: ", "%.2f", armLift);
+            telemetry.addData("armLift Encoder Position: ", position);
+            telemetry.update();
+        }
+    }
+}
