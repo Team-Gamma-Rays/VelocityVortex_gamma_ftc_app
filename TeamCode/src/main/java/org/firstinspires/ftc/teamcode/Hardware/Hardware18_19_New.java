7497897fd22413e3a55c360ef5f4972246644678
Gamma Rays FTC Team #9937 <ftcgammarays@gmail.com>
2018-09-25 15:33:09 -0400

    Create Hardware Map class for our first robot prototype of the 2018-2019 season
    - Richie

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Hardware18_19_New.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Hardware18_19_New {
+
+    public DcMotor leftFrontDrive = null;
+    public DcMotor rightFrontDrive = null;
+    public DcMotor leftRearDrive = null;
+    public DcMotor rightRearDrive = null;
+
+    public static final double DRIVE_POWER = 1.0;
+
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public Hardware18_19_New() {
+
+    }
+
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+
+        leftFrontDrive = hwMap.get(DcMotor.class, "left_front_drive");
+        rightFrontDrive = hwMap.get(DcMotor.class, "right_front_drive");
+        leftRearDrive = hwMap.get(DcMotor.class, "left_rear_drive");
+        rightRearDrive = hwMap.get(DcMotor.class, "right_rear_drive");
+
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftRearDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightRearDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        leftRearDrive.setPower(0);
+        rightRearDrive.setPower(0);
+
+        /*
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftRearDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightRearDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        */
+
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+}
